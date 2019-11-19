c00694a47f6465e46ae488fc6a3609a6f5a2ee33
himawantan <himawan.tan.b@gmail.com>
2019-11-19 13:45:06 +0700

    Updates

---
+++ precache-manifest.ea74c6d4f7828d4045397a3dbb7042cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc626ad44cc645d55f49cf3e402e0df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "03bddef9742ebcab6560",
+    "url": "/monsters-rolodex/static/css/main.33927979.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "03bddef9742ebcab6560",
+    "url": "/monsters-rolodex/static/js/main.cfff28dd.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
